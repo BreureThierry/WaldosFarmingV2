71ba7f09b15ed2dfd9f7a6d8fd9c2ff50578e7a0
Breure Thierry <43873548+BreureThierry@users.noreply.github.com>
2024-04-07 20:30:53 +0200

    Update plantations.js fix unknown interaction 2

--- commands/game/plantations.js
+++ commands/game/plantations.js
@@ -1016,8 +1016,22 @@ module.exports = {
                 await buttonInteraction.update({ embeds: [embed2], components: [row] });
                 return;
             }
-            // Met à jour le message
-            await buttonInteraction.update({ embeds: [slotEmbed], components: [slotRow, slotRow2], fetchReply: true  });
+            try {
+                // Met à jour le message
+                if (!buttonInteraction.replied) {
+                    await buttonInteraction.update({ embeds: [slotEmbed], components: [slotRow, slotRow2], fetchReply: true  });
+                    return;
+                } else {
+                    console.log("Interaction déjà répondu ou expiré");
+                }
+            } catch (error) {
+                if (error instanceof DiscordAPIError && error.code === 10062) {
+                    console.log("L'interaction est inconnue ou a expiré");
+                } else {
+                    // Gérer les autres types d'erreurs
+                    console.error(error);
+                }
+            }
         });
 
         collector.on('end', (message) => {
